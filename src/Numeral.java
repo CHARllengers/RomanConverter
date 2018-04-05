1223a5fe23e89e161daf2589186acb3a8bb7b5c6
nzhe4831 <nzhe4831@uni.sydney.edu.au>
2018-04-05 21:54:16 +1000

    Merging roman to arabic method manually

--- src/Numeral.java
+++ src/Numeral.java
@@ -1,16 +1,29 @@
 import java.util.*;
 
 public enum Numeral {
-	
+
 	I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
 	int weight;
 
 	Numeral(int weight) {
 		this.weight = weight;
 	}
+	final static Numeral[] values = Numeral.values();
 
+	//Checks if the Roman numerals inserted are valid
+	public static boolean isRoman(String n){
+
+		for(int i = 0; i < n.length(); i++){
+			//			char cursor = n.charAt(i);
+			if(n.charAt(i) != 'I' && n.charAt(i) != 'V' && n.charAt(i) != 'X' && n.charAt(i) != 'L' &&
+					n.charAt(i) != 'C' && n.charAt(i) != 'D' && n.charAt(i) != 'M'){
+				return false;
+			}
+		}
+		return true;
+	}
 	//method to convert Arabic to Roman
-	public static String roman(long n) {
+	public static String roman(int n) {
 
 		if( n <= 0) {
 			throw new IllegalArgumentException();
@@ -18,7 +31,6 @@ public enum Numeral {
 
 		StringBuilder buf = new StringBuilder();
 
-		final Numeral[] values = Numeral.values();
 
 		for (int i = values.length - 1; i >= 0; i--) {
 			while (n >= values[i].weight) {
@@ -26,17 +38,11 @@ public enum Numeral {
 				n -= values[i].weight;
 			}
 		}
-		System.out.println(buf.toString());
 		return buf.toString();
-		
 	}
-	static int count=0;
-	
+
 	//method to convert Roman to Arabic
-	public static String arabic(String line){
-		if( line==null) {
-			throw new IllegalArgumentException();
-		}
+		public static long arabic(String line){
 
 	
 
@@ -77,22 +83,43 @@ public enum Numeral {
 		System.out.print(arabic);
 		
 		return null;
+		//TODO
+		return 0;
 	}
-	
+
 	//prints Arabic
 	public static void RomantoArabic(String n) {
 		System.out.println(n + " = " + arabic(n));
 	}
-	
+
 	//prints Roman
-	public static void ArabictoRoman(long n) {
+	public static void ArabictoRoman(int n) {
 		System.out.println(n + " = " + roman(n));
 	}
 
 	public static void main(String[] args) {
-		//TODO
-		arabic("XLIV");
-		System.out.println("");
+
+		System.out.println("What would you like to convert:(a/b)\n\n"
+				+ "a.Roman to Arabic numerals\nb.Arabic to Roman numerals");
+		Scanner input = new Scanner(System.in);
+		String choice = input.next();
+
+		if(choice.compareTo("a") == 0){
+			System.out.print("Insert Roman Numerals:");
+			String romans = input.next();
+			//check for valid Roman numerals
+			while(!isRoman(romans)){
+				System.out.println("Please insert valid line");
+				romans = input.next();
+			}
+			RomantoArabic(romans);
+		}else if(choice.compareTo("b") == 0){
+			System.out.print("Insert Arabic Numerals:");
+			int arabics = input.nextInt();
+			ArabictoRoman(arabics);
+		}else{
+			System.out.println("goodbye");
+		}
 	}
 
 }
